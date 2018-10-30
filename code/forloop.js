5e915c7c2b7c8b28353c31b84ced98726592032a
Lbh <544289495@qq.com>
2018-10-30 11:58:23 +0800

    补充说明+几个小案例

---
+++ code/forloop.js
@@ -0,0 +1,20 @@
+//forloop.js
+
+// var fs = require('fs');
+// var files = ['a.txt','b.txt','c.txt'];
+// for(var i = 0; i < files.length; i++){
+// 	fs.readFile(files[i],'utf-8',function(err,contents){
+// 		console.log(files[i]+ ':' + contents);
+// 	});
+// }
+
+
+var fs = require('fs');
+var files = ['a.txt','b.txt','c.txt'];
+for(var i = 0; i < files.length; i++){
+	(function(i){
+		fs.readFile(files[i],'utf-8',function(err,contents){
+			console.log(files[i]+ ':' + contents);
+		});
+	})(i)
+}
